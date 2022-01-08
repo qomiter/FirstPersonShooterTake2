ff7141e7b36ce44bc7914b8e1098724849708dff
qomiter <qomiter@gmail.com>
2022-01-08 01:16:00 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+
+    public Transform camTrans;
+
+    public float mouseSensitivity;
+
+    public bool invertX;
+
+    public bool invertY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        // moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+        moveInput = moveInput * moveSpeed;
+
+        
+        charCon.Move(moveInput * Time.deltaTime);
+
+        //control camera rotation
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        if (invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+        if (invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FirstPersonShooterTake2
+ 
