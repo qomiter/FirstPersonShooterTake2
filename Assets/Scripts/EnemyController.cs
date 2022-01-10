a98778318e13043f5c9c61d505dcbf3ca823e36e
qomiter <qomiter@gmail.com>
2022-01-10 17:01:32 -0500

    tweaks

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,23 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody theRB;
+    //public Rigidbody theRB;
+    private bool chasing;
+    public float distanceToChase = 10f, distanceToLose = 15f, distanceToStop = 2f;
+
+    public NavMeshAgent agent;
+
+    private Vector3 targetPoint, startPoint;
+
+    public float keepChasingTime;
+    private float chaseCounter;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(PlayerController.instance.transform.position);
+        targetPoint = PlayerController.instance.transform.position;
+        targetPoint.y = transform.position.y;
 
-        theRB.velocity = transform.forward * moveSpeed;
+        if (!chasing)
+        {
+            if (Vector3.Distance(transform.position, targetPoint) < distanceToChase)
+            {
+                chasing = true;
+            }
+            if (chaseCounter > 0)
+            {
+                chaseCounter -= Time.deltaTime;
+
+                if (chaseCounter <= 0)
+                {
+                    agent.destination = startPoint;
+                }
+            }
+        }
+        else
+        {
+            //transform.LookAt(targetPoint);
+
+            //theRB.velocity = transform.forward * moveSpeed;
+
+            if(Vector3.Distance(transform.position, targetPoint) > distanceToStop)
+            {
+                agent.destination = targetPoint;
+            }
+            else
+            {
+                agent.destination = transform.position;
+            }
+            
+            if (Vector3.Distance(transform.position, targetPoint) > distanceToLose)
+            {
+                chasing = false;
+
+                chaseCounter = keepChasingTime;
+            }
+        }
     }
 }
