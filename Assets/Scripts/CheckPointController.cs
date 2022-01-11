6c49ae61d5c387407f7d2436711cbddacc76471e
qomiter <qomiter@gmail.com>
2022-01-10 20:11:30 -0500

    update

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckPointController : MonoBehaviour
+{
+    public string cpName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey(SceneManager.GetActiveScene().name + "_cp"))
+        {
+            if(PlayerPrefs.GetString(SceneManager.GetActiveScene().name + "_cp") == cpName)
+            {
+                PlayerController.instance.transform.position = transform.position;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", cpName);
+        }
+    }
+}
